f32b6c1990f6f2584e65ba41d689a8f3f309344d
Gangadhar K <gangadhark@monocept.com>
2024-09-28 20:48:36 +0530

    leads changes

--- src/app/services/leads.service.ts
+++ src/app/services/leads.service.ts
@@ -1,15 +1,17 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { ConfigService } from 'src/app/config.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LeadsService {
 
-  constructor(private http:HttpClient) { }
+  constructor(private http:HttpClient, private configService: ConfigService) { }
 
   getLeadsListApi(requestBody:any):Observable<any>{
-    return this.http.post(`https://usp.monocept.ai/yatra/api/getLeadDetailsList`, requestBody);
+    const getProposalListApi = this.configService.config.baseUrl + this.configService.config.getLeadsListApi;
+    return this.http.post<any>(getProposalListApi, requestBody);
   }
 }
