0b0ee33519324409ede382be042337327db22f48
IbbaVenkatesh <ivenkatesh@monocept.com>
2024-10-10 19:28:27 +0530

    Modified endorsements requests payload and integrated endorsementsCaseDetails API with hosted URL

--- src/app/endorsements/endorsement-details/endorsement-details.component.ts
+++ src/app/endorsements/endorsement-details/endorsement-details.component.ts
@@ -32,9 +32,8 @@ export class EndorsementDetailsComponent {
     this.endorsement_service
       .endorsementCaseDetailsApi(endorsementCaseDetailsReqBody)
       .subscribe(
-        (resp) => {
-          console.log(resp);
-          this.policyData = resp;
+        (resp:any) => {
+          this.policyData = resp.data;
         },
         (err: any) => {
           console.log(err);        }
