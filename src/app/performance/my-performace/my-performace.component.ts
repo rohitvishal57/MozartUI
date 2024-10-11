af9813dd7fbcbb34ddd001a0a871292e78c928d4
AzureAD\SomaAjayKrishna <ajaykrishnasoma@monocept.com>
2024-10-11 14:40:59 +0530

    toggle button changes added

--- src/app/performance/my-performace/my-performace.component.ts
+++ src/app/performance/my-performace/my-performace.component.ts
@@ -9,9 +9,12 @@ export class MyPerformaceComponent {
   selectedTabIndex: number = 0;
   docType = 'Monthly';
   detailedList: any;
-  selectedView: string = "list";
+  selectedView: string = "Monthly";
   onTabChanged(event: any): void {
     this.selectedTabIndex = event.index;
   }
-
+  toggleView(key: any){
+    console.log(key);
+    this.selectedView = key;
+  }
 }
