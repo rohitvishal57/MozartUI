5f2b47cc155c7ca09e92155b0c1d20ebdd8bb3bb
sukhadev <ksukhadev@monocept.com>
2024-10-16 08:21:27 +0530

    encrypt and decrypt in service file

--- src/app/services/encryption.service.ts
+++ src/app/services/encryption.service.ts
@@ -5,7 +5,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class EncryptionService {
-  private secretKey = 'BANCA123456ASSURANCE54321PROJECT';
+  private secretKey = '0123456789abcdef0123456789abcdef';
 
   encrypt(data: any) {
     const encryptedData = CryptoJS.AES.encrypt(
@@ -22,4 +22,22 @@ export class EncryptionService {
     ).toString(CryptoJS.enc.Utf8);
     return JSON.parse(decryptedData);
   }
+
+
+  textEncrypt$(data: string) {
+    return CryptoJS.AES.encrypt(data, this.secretKey.trim()).toString()
+  }
+
+  textDecrpt$(data: string) {
+    return CryptoJS.AES.decrypt(data, this.secretKey.trim()).toString(CryptoJS.enc.Utf8);
+  }
+
+  objEncrypt$(data: any) {
+    return CryptoJS.AES.encrypt(JSON.stringify(data), this.secretKey.trim()).toString();
+  }
+  objDecrpt$(data: any) {
+    const decrypted = CryptoJS.AES.decrypt(data, this.secretKey.trim());
+    return CryptoJS.enc.Utf8.stringify(decrypted)
+  }
+
 }
