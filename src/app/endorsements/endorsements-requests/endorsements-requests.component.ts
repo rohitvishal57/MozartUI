e7c3c15cc9f841b4d46573959a1aaa16c3a60a97
IbbaVenkatesh <ivenkatesh@monocept.com>
2024-10-18 14:04:53 +0530

    Bug Fixes: Resolved input validations

--- src/app/endorsements/endorsements-requests/endorsements-requests.component.ts
+++ src/app/endorsements/endorsements-requests/endorsements-requests.component.ts
@@ -3,6 +3,8 @@ import { FormControl, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { endorsementDetails } from 'src/app/interface/endorsement.interface';
 import { EndorsementsRequestsService } from './endorsements-requests.service';
+import { DatePipe } from '@angular/common';
+import { CommonService } from 'src/app/services/common.service';
 
 @Component({
   selector: 'app-endorsements-requests',
@@ -25,15 +27,111 @@ export class EndorsementsRequestsComponent implements OnInit {
   isDesktopView: boolean = false
   fromDate: any;
   toDate: any;
+  toggeleSearchdropdown: boolean = false;
+  toggeledropdown: boolean = false;
+  appliedFiltersCount: number = 0;
+  StaticRequestTypes = [
+   
+    {
+      name: "Aadhar Card Update",
+      value: "aadharNumber",
+      selected: false 
+    },
+    {
+      name: "Pancard Update",
+      value: "panNumber",
+      selected: false
+    },
+    // {
+    //   name: "Change my Primary Registered Number",
+    //   value: "primaryContactNumber",
+    //   selected: false
+    // },
+    // {
+    //   name: "Change my Alternate number",
+    //   value: "alternateContactNumber",
+    //   selected: false
+    // },
+    // {
+    //   name: "Change in my Email ID",
+    //   value: "email",
+    //   selected: false
+    // },
+    // {
+    //   name: "Change my Alternate Email ID",
+    //   value: "alternateEmail",
+    //   selected: false
+    // },
+    // {
+    //   name: "Change my Primary Registered Number- Member",
+    //   value: "memberPrimaryContactNumber",
+    //   selected: false
+    // },
+    // {
+    //   name: "Change my Alternate number- member",
+    //   value: "memberAlternateContactNumber",
+    //   selected: false
+    // },
+    // {
+    //   name: "Change in my Email ID- member",
+    //   value: "memberEmail",
+    //   selected: false
+    // },
+    // {
+    //   name: "Change my Alternate Email ID - member",
+    //   value: "memberAlternateEmail",
+    //   selected: false
+    // },
+    // {
+    //   name: "Change of Nominee",
+    //   value: "nomineeContact",
+    //   selected: false
+    // },
+    // {
+    //   name: "Change in International Contact Number",
+    //   value: "internationalContactNumber",
+    //   selected: false
+    // },  
+    // {
+    //   name: "Change in International Address",
+    //   value: "ChangeinInternationalAddress",
+    //   selected: false
+    // },
+
+  ];
+  agentCode: any;
+  productsList: any = [];
+  requestTypes:any =[];
   constructor(private router:Router,
     private endorsementService: EndorsementsRequestsService,
     private _router: Router,
+    private datePipe: DatePipe,
+    private commonService: CommonService,
   ) { }
 
   ngOnInit(): void {
     this.getRequestList();
+    this.getProducts();
   }
-
+  getProducts() {
+    this.agentCode =  localStorage.getItem("agentCode");
+    const reqData={
+      "agentCode": this.agentCode
+    }
+    this.commonService.Getproductlist(reqData).subscribe({
+      next: (res:any) => {
+        this.productsList = res.data;
+        console.log("product list",this.productsList)
+        const uniqueRequestTypes = Array.from(new Set(this.productsList
+         .map((product:any) => product.familyPlan)))
+         .map((requestType) => ({ name: requestType, selected: false }));
+         this.requestTypes = uniqueRequestTypes;
+      },
+      error: (err:any) => {
+         console.log("error coming form getproduct list API");
+      }
+    })
+  }
   downloadRequest(data: any) {
     alert(data.status);
   }
@@ -110,7 +208,65 @@ export class EndorsementsRequestsComponent implements OnInit {
   getToDate(event:any){
     this.toDate = event.target.value;
   }
-
+  formatDate(dateType: "fromDate" | "toDate") {
+    if (dateType === "fromDate" && this.fromDate) {
+      this.fromDate = this.datePipe.transform(this.fromDate, "yyyy-MM-dd");
+    } else if (dateType === "toDate" && this.toDate) {
+      this.toDate = this.datePipe.transform(this.toDate, "yyyy-MM-dd");
+    }
+  }
+  toggleFilterDropdown() {
+    if(this.toggeleSearchdropdown==true)
+    {
+       this.toggeleSearchdropdown=false;
+    }
+    this.toggeledropdown = !this.toggeledropdown;    
+  }
+  cancel() {
+    this.fromDate = null;
+    this.toDate = null;
+    this.requestsListRequestBody.fromDate = '';
+    this.requestsListRequestBody.toDate = '';
+    this.toggeledropdown = false;
+    this.getRequestList();
+  }
+  calculateAppliedFiltersCount(){
+    const selectedPolicyTypesCount = this.StaticRequestTypes.filter(
+      (requestType:any) => requestType.selected).length;
+      // const selectedProductsCount = this.productsList.filter(
+      //   (product) => product.selected).length;
+        let count = selectedPolicyTypesCount;
+        if (this.fromDate && this.toDate) {
+          count++;
+        }
+        this.appliedFiltersCount = count;
+         this.appliedFiltersCount;
+  }
+  clear(){
+    this.productsList.forEach((product:any) => (product.selected = false));
+    this.StaticRequestTypes.forEach((requestType) => (requestType.selected = false));
+    this.fromDate = null;
+    this.toDate = null;
+    this.appliedFiltersCount = 0;
+   // this.claimsReqBody.productName = "";
+    this.requestsListRequestBody.requestType = "";
+    this.requestsListRequestBody.fromDate = "";
+    this.requestsListRequestBody.toDate = "";
+    this.getRequestList();
+  }
+  applyFilter() {
+    this.calculateAppliedFiltersCount();
+  this.formatDate("fromDate");
+  this.formatDate("toDate");
+  this.requestsListRequestBody.fromDate=this.fromDate;
+  this.requestsListRequestBody.toDate=this.toDate;
+  const selectedPolicyTypes = this.StaticRequestTypes
+  .filter((requestType:any) => requestType.selected)
+  .map((requestType:any) => requestType.name);
+  // this.requestsListRequestBody.requestType = selectedPolicyTypes.join(", ");
+  this.getRequestList();
+  this.toggeledropdown=false;
+}
   onSelectChanges(event: Event): void {
     const inputElement = event.target as HTMLInputElement;
     this.selected = inputElement.value;
