75f02a79d40dd5fa9da327030d8862ccfc045531
kumar raj <52935294+kumarmonocept@users.noreply.github.com>
2024-10-25 14:09:29 +0530

    Date fix in create lead

--- src/app/leads/create-lead/create-lead.component.ts
+++ src/app/leads/create-lead/create-lead.component.ts
@@ -38,6 +38,7 @@ export class CreateLeadComponent implements OnInit {
   referenceStatus: any;
   referenceSubStatus: any;
   activityTypes: any = [];
+  today:string='';
   constructor(private formBuilder: FormBuilder,
     private toast: NgToastService,
     private router: Router,
@@ -92,7 +93,8 @@ export class CreateLeadComponent implements OnInit {
     //   }
     // );
 
-  
+    const date = new Date();
+    this.today = date.toISOString().split('T')[0];
   }
 
   inItForm() {
