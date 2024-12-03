4e744041868127de209bbff1bce0ecac17ca781f
kumar raj <52935294+kumarmonocept@users.noreply.github.com>
2024-12-03 12:47:11 +0530

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -28,6 +28,9 @@ import { NotificationsComponent } from './notifications/notifications.component'
 import { SharedModule } from './shared/shared.module';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
+import { LocationStrategy, PathLocationStrategy } from '@angular/common';  // Import LocationStrategy and PathLocationStrategy
+
+
 export function loadConfig(configService: ConfigService) {
   return () => configService.loadConfig();
 }
@@ -73,6 +76,8 @@ export function HttpLoaderFactory(http: HttpClient) {
   providers: [
     ConfigService,
     LoadingService,
+    { provide: LocationStrategy, useClass: PathLocationStrategy },  // Use PathLocationStrategy
+
     {
       provide: APP_INITIALIZER,
       useFactory: loadConfig,
