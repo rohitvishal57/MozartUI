da6e1cde797a22c8ce9551e7d47ac4473f2d5d89
manikanta-monocept <manikanta@monocept.com>
2024-12-06 15:08:07 +0530

    sidebar nav overlay issue fixed

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -125,6 +125,7 @@ export class HeaderComponent implements OnInit ,OnDestroy {
   }
 
   onClickHamburger(ev : any){
+    console.log(this.loginService.getValue())
     this.loginService.toggleSidebar(!this.loginService.getValue());
   }
 
