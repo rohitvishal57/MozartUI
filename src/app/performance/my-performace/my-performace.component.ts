7ae8949c7f107cf0802e89bef124306f0fcdd2a2
kumar raj <52935294+kumarmonocept@users.noreply.github.com>
2024-12-12 18:32:40 +0530

    Commission Statement changes into MyPerformance.

--- src/app/performance/my-performace/my-performace.component.ts
+++ src/app/performance/my-performace/my-performace.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { PerformanceService } from '../performance.service';
 import { LanguageService } from 'src/app/services/language.service';
 import { TranslateService } from '@ngx-translate/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { NgToastService } from 'ng-angular-popup';
 
 @Component({
   selector: 'app-my-performace',
@@ -21,12 +23,28 @@ export class MyPerformaceComponent {
   first: number = 0;
   rows: number = 10;
   agentCode :any = '';
+  commissionForm!: FormGroup;
+  years: any;
+  months: any = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+  cycles: any = [
+    {
+      "name": "Cycle: 1st (1st to 15th of the month)",
+      "value": "MID"
+    },
+    {
+      "name": "Cycle: 2nd (16th to End of the month)",
+      "value": "END"
+    }
+  ];
+
   constructor(
     private performanceService: PerformanceService, private languageService: LanguageService,
-    private translateService: TranslateService
-
+    private translateService: TranslateService,private formBuilder: FormBuilder,private toast: NgToastService
   ) { }
   ngOnInit(): void {
+    const currentYear = new Date().getFullYear();
+    this.years = [currentYear, currentYear - 1]
+    this.inItForm();
     this.languageService.language$.subscribe(lang => {
       this.translateService.use(lang).subscribe({
         error: () => {
@@ -41,6 +59,14 @@ export class MyPerformaceComponent {
     this.getPerformanceData();
     this.getPerformanceDetailedViewCount();
     this.getPerformanceDetailedList();
+  }
+
+  inItForm() {
+    this.commissionForm = this.formBuilder.group({
+      year: [''],
+      month: [''],
+      cycle: ['']
+    });
   }
 
   getPerformanceData() {
@@ -109,5 +135,74 @@ export class MyPerformaceComponent {
   toggleView(key: any){
     console.log(key);
     this.docType = key;
+  }
+
+
+  fetchCommissionStatement() {
+    let requestbody: any = this.commissionForm.getRawValue();
+    requestbody.agentCode = this.agentCode;
+    requestbody.type = 'CommissionStatement';
+    this.performanceService.fetchCommissionStatement(requestbody).subscribe(
+      (respose) => {
+        if (respose.isSuccess) {
+          let commissionDetails = respose?.data?.searchResponse[0];
+          if(commissionDetails.fileName && commissionDetails.omniDocIndex){
+            this.downloadStatement(commissionDetails.omniDocIndex, commissionDetails.fileName);
+          }else{
+            this.toast.warning({ detail: "", summary: 'There are no commission statements to download.', duration: 2000 }); 
+          }
+        }else{
+          this.toast.warning({ detail: "", summary: 'Failed to fetch commission statement.', duration: 2000 });
+        }
+      },
+      (error) => {
+        console.log('Failed to fetch commission statement', error);
+        this.toast.error({ detail: "", summary: 'Failed to fetch commission statement.', duration: 2000 });
+      });
+  }
+
+  downloadStatement(omniDocImageIndex: string, fileName: string) {
+    let requestBody: any = {};
+    requestBody.agentCode =  this.agentCode;
+    requestBody.downloadRequest = [{
+      omniDocImageIndex: omniDocImageIndex,
+      fileName: fileName
+    }];
+
+    this.performanceService.downloadCommissionStatement(requestBody).subscribe(
+      (response) => {
+        try{
+          if (response.isSuccess) {
+            const blob = this.base64ToBlob(response?.data?.downloadResponse[0]?.byteArray, 'application/pdf');
+            const url = window.URL.createObjectURL(blob);
+            const link = document.createElement('a');
+            link.href = url;
+            link.download = fileName;
+            document.body.appendChild(link);
+            link.click();
+            document.body.removeChild(link);
+            window.URL.revokeObjectURL(url);
+            this.toast.success({ detail: "", summary: 'Commission Statement Downloaded Successfully.', duration: 2000 }); 
+          }else{
+            this.toast.warning({ detail: "", summary: 'Failed to download commission statement.', duration: 2000 });
+          }
+        }catch(error){
+          console.log('errror download pdf',error);
+        }
+      }, (error) => {
+        console.log('Failed to download commission statement', error);
+        this.toast.error({ detail: "", summary: 'Failed to download commission statement.', duration: 2000 });
+      });
+
+  }
+
+  base64ToBlob(base64: string, type: string): Blob {
+    const binary = atob(base64);
+    const length = binary.length;
+    const arrayBuffer = new Uint8Array(length);
+    for (let i = 0; i < length; i++) {
+      arrayBuffer[i] = binary.charCodeAt(i);
+    }
+    return new Blob([arrayBuffer], { type });
   }
 }
