aa1b602e5d78adba55d9b259cb095b212b7b0c4c
kumar raj <52935294+kumarmonocept@users.noreply.github.com>
2024-12-12 18:35:42 +0530

    Removed Commission From Side Nav.

--- src/app/layout/side-navbar/side-navbar.component.ts
+++ src/app/layout/side-navbar/side-navbar.component.ts
@@ -25,7 +25,7 @@ export class SideNavbarComponent {
     { id: 9, displayName: 'My Performance', path: 'performance/my-performance', imagePath: 'assets/Img/icon_menu_performance.png' },
     // { id: 10, displayName: 'Upload Report', path: 'performance/upload-performance', imagePath: 'assets/Img/icon_menu_uploadreports.png' },
     { id: 11, displayName: 'Events', path: 'events/eventsList', imagePath: 'assets/Img/icon_menu_events.png' },
-    { id: 12, displayName: 'My Commissions', path: '/mycommissions', imagePath: 'assets/Img/icon_menu_commissionstatment.png' }
+   // { id: 12, displayName: 'My Commissions', path: '/mycommissions', imagePath: 'assets/Img/icon_menu_commissionstatment.png' }
 
   ];
 
