609680e0a8e706a3285bc41866133ddc21c7c184
Pratik Jadhav <pratikjadhav@monocept.com>
2024-12-13 19:49:36 +0530

    changes in getFullQuoteViaOfflinePayment function

--- src/app/yatra/yatra/yatra.component.ts
+++ src/app/yatra/yatra/yatra.component.ts
@@ -5756,6 +5756,7 @@ export class YatraComponent {
         policyNumber: "".toString(),
         agentCode: (this.agentCode || '').toString(),
         bankName: (this.formData?.bankName).toString(),
+        bankAccountNo:(this.formData?.accountNumber).toString(),
         IFSC: (this.formData?.ifscCode || '').toString(),
         micrNo: (this.formData?.micrCode || '').toString(),
         instrumentType: (this.formData.paymentOption || '').toString(),
