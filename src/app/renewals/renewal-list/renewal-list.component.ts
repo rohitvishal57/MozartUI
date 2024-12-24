7cc2dc61be15fc092d56ce89be365c6e4dd44769
PREMKUMAR ANBU <prem@monocept.com>
2024-12-24 18:45:11 +0530

    update E-Nach payment functionality

--- src/app/renewals/renewal-list/renewal-list.component.ts
+++ src/app/renewals/renewal-list/renewal-list.component.ts
@@ -187,9 +187,9 @@ export class RenewalListComponent {
       if (stateData.paymentStatus) {
         const paymentStatus = this.encryptionService.decrypt(stateData.paymentStatus);
         if(paymentStatus == "PENDING"){
-          this.toast.warning({detail: "SUCCESS",summary: "payment Pending",duration: 5000});
+          this.toast.warning({detail: "Warning",summary: "payment Pending",duration: 5000});
         }else if(paymentStatus == "INPROGRESS"){
-          this.toast.warning({detail: "SUCCESS",summary: "payment inprogress",duration: 5000});
+          this.toast.warning({detail: "Warning",summary: "payment inprogress",duration: 5000});
         }
       }
       try{
