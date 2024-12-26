26d82674e0f650e910d2493d2afc7acfa5b109db
Anekant Madrap <150224647+anekant-monocept@users.noreply.github.com>
2024-12-26 11:44:34 +0530

    payment document error fix

--- src/app/yatra/yatra/yatra.component.ts
+++ src/app/yatra/yatra/yatra.component.ts
@@ -3475,6 +3475,12 @@ export class YatraComponent {
                 .find((ctrl: any) => ctrl.name === item);
               if (controlToHide) {
                 controlToHide.visible = false; // Hide dependent controls for offline
+                const formControl = this.dynamicFormGroup.get(controlToHide.name);
+              if (formControl) {
+                formControl.disable();
+                formControl.clearValidators();
+                formControl.updateValueAndValidity();
+              }
               }
             });
           }
@@ -3490,6 +3496,12 @@ export class YatraComponent {
                 .find((ctrl: any) => ctrl.name === item);
               if (controlToHide) {
                 controlToHide.visible = false; // Hide dependent controls for other buttons
+                const formControl = this.dynamicFormGroup.get(controlToHide.name);
+              if (formControl) {
+                formControl.disable();
+                formControl.clearValidators();
+                formControl.updateValueAndValidity();
+              }
               }
             });
           }
