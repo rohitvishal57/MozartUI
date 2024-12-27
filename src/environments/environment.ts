40b3c17b1d541e78398d99fb79d64c555c25afdb
ksukhadev-Monocept <ksukhadev@monocept.com>
2024-12-27 11:27:12 +0530

    env should be same

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -185,5 +185,17 @@ export const environment = {
   saveTSCommonDraft:"api/v1/SaveTSCommonDraft",
   updateAv:"api/v1/CreateUpdateAV",
   deleteAV:"api/v1/DeleteAV",
-  bulkUpload:"api/v1/BulkUpload"
+  bulkUpload:"api/v1/BulkUpload",
+  fetchPennyVerificationByOCR: "api/yatra/pennydropverificationviaocr",
+  getInsertproposerdocumentid: "api/yatra/insertproposerdocumentid",
+  sCreateLead: "api/v1/SaveLeadDetails",
+  getAllManageLOB: "api/v1/GetAllManageLOB",
+  getPincodeDetails:"api/rug/GetPincodeDetails",
+  tsCreateLead: "api/v1/SaveLeadDetails",
+  getAllDispositions: "api/v1/getAllDispositions?appName=TELESALES",
+  getAllSubDispositions:'api/v1/GetAllSubDisposition',
+  saveTSCommonDraft:"api/v1/SaveTSCommonDraft",
+  postTSHalfQuote:"api/v1/PostHalfQuote",
+  getTSPolicyInfoByLeadId:"api/v1/GetTSPolicyInfoByLeadId",
+  bbHalfQuote: "api/rug/HalfQuote"
 };
