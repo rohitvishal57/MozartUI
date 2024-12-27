e15535cc3b85d28fa473340d51d4646efd0b58d3
PREMKUMAR ANBU <prem@monocept.com>
2024-12-27 19:04:51 +0530

    Skip KYC integration for renewal

--- src/app/renewals/renewal-journey/renewal-journey.component.ts
+++ src/app/renewals/renewal-journey/renewal-journey.component.ts
@@ -13,6 +13,7 @@ import { payment } from 'src/assets/styles/renewals-forms/payment';
 import { RenewalsService } from '../renewals.service';
 import { IFullQuoteMapping } from 'src/app/interface/FullQuote_Mapping.interface';
 import { Clipboard } from '@angular/cdk/clipboard';
+import { CustomersService } from 'src/app/customers/customers.service';
 
 @Component({
   selector: 'app-renewal-journey',
@@ -79,8 +80,11 @@ export class RenewalJourneyComponent {
   impressedLable: String = "";
   feedbackImpressedValue: String = '';
   isFeedBackModalVisible: Boolean = false;
+  retrievedDocuments: any;
+  verifyKYCStatus:boolean = false;
 
-  constructor(private route: ActivatedRoute, private encryptionService: EncryptionService, private renderer: Renderer2, @Inject(DOCUMENT) private document: Document, private yatraService: YatraService, private toast: NgToastService, public commonService: CommonService, private renewalService: RenewalsService, private router: Router, private clipboard: Clipboard) { }
+
+  constructor(private route: ActivatedRoute, private encryptionService: EncryptionService, private renderer: Renderer2, @Inject(DOCUMENT) private document: Document, private yatraService: YatraService, private toast: NgToastService, public commonService: CommonService, private renewalService: RenewalsService, private router: Router, private clipboard: Clipboard,private customerService: CustomersService) { }
 
   async ngOnInit() {
     this.showHtmlContent = false;
@@ -3711,7 +3715,12 @@ export class RenewalJourneyComponent {
   }
 
   checkKycDetail(control: any): void {
-    const isVisible = !(this.formData.isKycCompleted);
+    var isVisible = true;
+    if(this.verifyKYCStatus){
+      isVisible = false;
+    }else {
+      isVisible = !(this.formData.isKycCompleted);
+    }
     // const isVisible=true;
     this.form.formSections.forEach((section) => {
       section.formControls.forEach((formControl: IFormControl) => {
@@ -3866,6 +3875,141 @@ export class RenewalJourneyComponent {
 
   onSelectValue(value: String) {
     this.feedbackImpressedValue = value;
+  }
+
+  onClickDownloadFromConfirmation() {
+    this.onSearchDocumentFromConfirmation();
+    if (this.retrievedDocuments) {
+      const downloadPolicyKitRequestBody = {
+        agentCode: this.agentCode,
+        referenceId: this.agentCode,
+        eventName: "Download policy kit request from customers",
+        proposalNumber: this.policyNumber,
+        downloadRequest: [
+          {
+            omniDocImageIndex: this.retrievedDocuments[0].omniDocImageIndex,
+            fileName: this.retrievedDocuments[0].fileName,
+          },
+        ],
+        sourceSystemName: "",
+        identifier: "",
+      };
+      this.customerService.downloadDocumentApi(downloadPolicyKitRequestBody).subscribe(
+        (response: any) => {
+          if (response.isSuccess && response.data?.downloadResponse?.length > 0) {
+            const file = response.data.downloadResponse[0];
+            if (file.byteArray && file.fileName) {
+              const byteArray = new Uint8Array(
+                atob(file.byteArray).split("").map((char) => char.charCodeAt(0))
+              );
+              const blob = new Blob([byteArray], { type: "application/pdf" });
+              const fileURL = window.URL.createObjectURL(blob);
+              const link = document.createElement("a");
+              link.href = fileURL;
+              link.download = file.fileName;
+              document.body.appendChild(link)
+              link.click();
+              document.body.removeChild(link)
+              window.open(fileURL, "_blank");
+            }
+          } else {
+            this.toast.error({ detail: "", summary: response.message || "No file found to download.", duration: 3000 });
+          }
+        },
+        (error: any) => {
+          console.error("Download Policy Kit Error:", error);
+          this.toast.error({ detail: "", summary: "Error while downloading Policy Kit.", duration: 3000 });
+        }
+      );
+    }
+
+  }
+
+  onSearchDocumentFromConfirmation() {
+    const searchDocumentRequestBody = {
+      referenceId: this.agentCode,
+      searchRequest: [
+        {
+          categoryID: "",
+          description: "",
+          dataClassParam: [
+            {
+              docSearchParamId: "2",
+              value: this.policyNumber
+            },
+            {
+              docSearchParamId: "15",
+              value: "PS_04",
+            },
+          ],
+        },
+      ],
+      agentCode: this.agentCode,
+      eventName: "Search policy kit request from customers",
+      sourceSystemName: "",
+      searchOperator: "AND",
+    };
+    this.customerService.searchDocumentApi(searchDocumentRequestBody).subscribe(
+      (response: any) => {
+        if (response.isSuccess) {
+          const searchResponse = response.data.searchResponse;
+          this.retrievedDocuments = searchResponse;
+          if (searchResponse && searchResponse[0]?.error?.length > 0) {
+            this.toast.success({ detail: "", summary: "No documents are available to download.", duration: 2000 });
+            return;
+          }
+        } else {
+          this.toast.error({ detail: "", summary: response.message || "Failed to search document.", duration: 2000 });
+        }
+      },
+      (error: any) => {
+        console.error("Search document error", error);
+        this.toast.error({ detail: "", summary: "Error while searching the document.", duration: 2000 });
+      }
+    );
+  }
+
+  getLabels(control: any) {
+    let startIdx = control.indexOf('{{');
+    let endIdx = control.indexOf('}}');
+    let string: any;
+    if (startIdx !== -1 && endIdx !== -1) {
+      string = control.slice(startIdx + 2, endIdx).trim();
+    }
+
+    switch (string) {
+      case 'actName':
+        return control.replace('{{actName}}', this.formData?.accountNumber);
+      case 'emailId':
+        const emailId = this.formData?.emailId;
+        if (emailId) {
+          const anchorTag = `<a href="mailto:${emailId}">${emailId}</a>`;
+          return control.replace('{{emailId}}', anchorTag);
+        }
+        return control;
+      default:
+        return control;
+    }
+  }
+
+  skipKycURL(control:any){
+    const skipKycRequestBody = {
+      proposalOrPolicyNumber: this.policyNumber,
+      businessType:"REN"
+    };
+    this.renewalService.skipKycLinkApi(skipKycRequestBody).subscribe(
+      (res: any) => {
+        console.log("skipKycResponseBody", res);
+        if (res.data.kycStatus) {
+          this.toast.success({detail: "Success",summary: res.message,duration: 3000,});
+          this.verifyKYCStatus = res.data.kycStatus;
+          this.checkKycDetail(control);
+        }
+      },
+      (err) => {
+        console.log(err);
+      }
+    );
   }
 
 }
