6a6c43fd062abeae0479dcf1d88e2a1ce27ad3c6
vamsi-monocept <vamsi@monocept.com>
2025-01-02 12:35:29 +0530

    Implemented day click and Carousel in calendar UI

--- src/app/events/events-list/events-list.component.ts
+++ src/app/events/events-list/events-list.component.ts
@@ -7,7 +7,8 @@ import {
   addWeeks,
   subDays,
   subMonths,
-  subWeeks} from "date-fns";
+  subWeeks
+} from "date-fns";
 import { Subject } from "rxjs";
 import { Router } from "@angular/router";
 import { EventsService } from "../events-new/events.service";
@@ -15,7 +16,7 @@ import { MatDialog } from "@angular/material/dialog";
 import { CalendarEvent, CalendarView } from "src/app/interface/events.interface";
 import { TranslateService } from '@ngx-translate/core';
 import { LanguageService } from 'src/app/services/language.service';
-// import * as $ from 'jquery';
+import * as $ from 'jquery';
 
 
 @Component({
@@ -24,7 +25,7 @@ import { LanguageService } from 'src/app/services/language.service';
   styleUrls: ["./events-list.component.scss"],
 })
 export class EventsListComponent implements OnInit {
-  view: CalendarView = CalendarView.Day ;
+  view: CalendarView = CalendarView.Day;
   CalendarView = CalendarView;
   viewDate: Date = new Date();
   events: CalendarEvent[] = [];
@@ -33,26 +34,26 @@ export class EventsListComponent implements OnInit {
   dayStartHour: any;
   dayEndHour: any;
   selectedEvent: any = null;
-  // currentEventIndex: number = 0;
-  // selectedMonthRecords:any = [];
-  // slickConfig = {
-  //   dots: true,            // Enable dots
-  //   infinite: true,        // Infinite scrolling
-  //   speed: 500,            // Slide speed in milliseconds
-  //   slidesToShow: 1,       // Number of slides to show at a time
-  //   slidesToScroll: 1,     // Number of slides to scroll at a time
-  //   autoplay: true,        // Enable autoplay
-  //   autoplaySpeed: 2000,   // Set autoplay speed (2 seconds)
-  //   arrows: true,          // Show navigation arrows
-  //   centerMode: false      // Set to false if you don't need center mode
-  // };
+  currentEventIndex: number = 0;
+  selectedMonthRecords: any = [];
+  slickConfig = {
+    dots: false,
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 2500,
+    speed: 300,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true,
+  };
 
   @ViewChild('eventModal') eventModal: any;
-  constructor(private route: Router, private eventsService: EventsService,   private dialog: MatDialog, private languageService: LanguageService,
-    private translateService: TranslateService) {}
+  @ViewChild('eventModalCarousel') eventModalCarousel: any;
+  constructor(private route: Router, private eventsService: EventsService, private dialog: MatDialog, private languageService: LanguageService,
+    private translateService: TranslateService) { }
 
   ngOnInit(): void {
-    window.scrollTo(0, 0); // Scroll to top when the component is initialized
+    window.scrollTo(0, 0);
     this.languageService.language$.subscribe(lang => {
       this.translateService.use(lang).subscribe({
         error: () => {
@@ -63,22 +64,21 @@ export class EventsListComponent implements OnInit {
     this.loadEvents();
   }
 
-
   loadEvents(): void {
     const agentCode = localStorage.getItem("agentCode");
-    let getEventReq = 
-      {
-        "agentCode": agentCode,
-        "startDate": null,
-        "endDate": null,
-        "eventType": "",
-        "eventNumber": "",
-        "customerName": "",
-        "mobileNumber": "",
-        "pageNumber": 1,
-        "pageSize": 10
-      }
-  
+    let getEventReq =
+    {
+      "agentCode": agentCode,
+      "startDate": null,
+      "endDate": null,
+      "eventType": "",
+      "eventNumber": "",
+      "customerName": "",
+      "mobileNumber": "",
+      "pageNumber": 1,
+      "pageSize": 10
+    }
+
     this.eventsService.getEvents(getEventReq, agentCode).subscribe(
       (response: any) => {
         if (response.isSuccess) {
@@ -87,12 +87,12 @@ export class EventsListComponent implements OnInit {
             return event.eventSchedule.map((schedule: any) => {
               const startDateTime = this.parseDateTime(schedule.date, schedule.startTime);
               const endDateTime = this.parseDateTime(schedule.date, schedule.endTime);
-  
+
               if (!startDateTime || !endDateTime) {
                 console.error("Invalid date/time for event:", event);
                 return null;
               }
-  
+
               return {
                 note: event.note,
                 title: `${event.customerName} </br> ${event.activityType}  </br>  ${event.note}`,
@@ -111,19 +111,19 @@ export class EventsListComponent implements OnInit {
               };
             });
           })
-          // Flatten the array of arrays since we mapped event schedules
-          .flat()
-          // Remove any null events from invalid dates
-          .filter((event: any) => event !== null);
-  
+            // Flatten the array of arrays since we mapped event schedules
+            .flat()
+            // Remove any null events from invalid dates
+            .filter((event: any) => event !== null);
+
           if (this.events.length > 0) {
             const startHours = this.events.map(event => event.start.getHours());
             const endHours = this.events.map(event => event.end.getHours());
-            
+
             this.dayStartHour = Math.min(...startHours);
             this.dayEndHour = Math.max(...endHours);
           }
-  
+
           this.refresh.next();
         } else {
           console.error("Error loading events:", response.message);
@@ -135,14 +135,14 @@ export class EventsListComponent implements OnInit {
       }
     );
   }
-  
+
   parseDateTime(date: string, time: string): Date | null {
     try {
-     
+
       const cleanTime = time.split('.')[0];  // Remove any milliseconds from the time string if present
       const dateTimeStr = `${date}T${cleanTime}`; // Combine date and time
       const dateTime = new Date(dateTimeStr);
-  
+
       if (isNaN(dateTime.getTime())) {      // Validate the parsed date
         console.error(`Invalid DateTime: ${dateTimeStr}`);
         return null;
@@ -194,35 +194,19 @@ export class EventsListComponent implements OnInit {
   }
 
   handleEventClick(event: any) {
+    console.log(event.event);
+
     this.selectedEvent = event.event;
     this.openEventModal();
   }
   // handleEventClick(eventInfo: { event: any }) {
-    // const selectedEvents = this.events.filter(
-    //   (event) =>
-    //     event.start.getDate() === eventInfo.event.start.getDate() &&
-    //     event.start.getMonth() === eventInfo.event.start.getMonth() &&
-    //     event.start.getFullYear() === eventInfo.event.start.getFullYear()
-    // );
-  
-    // if (selectedEvents.length > 0) {
-    //   let selectedIndex = selectedEvents.findIndex(
-    //     (event) => event === eventInfo.event
-    //   );
-    //   selectedIndex = (selectedIndex + 1) % selectedEvents.length;
-    //   this.selectedEvent = selectedEvents[selectedIndex];
-    //   this.openEventModal();
-    // }
-  //}
-  // handleDayClick(eventInfo:any){
-  //   console.log(eventInfo.day.events,'data')
   //   const selectedEvents = this.events.filter(
   //     (event) =>
-  //       event.start.getDate() === eventInfo.day.events.start.getDate() &&
-  //       event.start.getMonth() === eventInfo.day.events.start.getMonth() &&
-  //       event.start.getFullYear() === eventInfo.day.events.start.getFullYear()
+  //       event.start.getDate() === eventInfo.event.start.getDate() &&
+  //       event.start.getMonth() === eventInfo.event.start.getMonth() &&
+  //       event.start.getFullYear() === eventInfo.event.start.getFullYear()
   //   );
-  
+
   //   if (selectedEvents.length > 0) {
   //     let selectedIndex = selectedEvents.findIndex(
   //       (event) => event === eventInfo.event
@@ -232,34 +216,59 @@ export class EventsListComponent implements OnInit {
   //     this.openEventModal();
   //   }
   // }
-  // addSlide() {
-  //   this.events.push(this.eventModal);
-  // }
-  // removeSlide() {
-  //   this.events.length = this.events.length - 1;
-  // }
-  // slickInit(e: any) {
-  //   console.log('slick initialized');
-  // }
-  // breakpoint(e: any) {
-  //   console.log('breakpoint');
-  // }
-  // afterChange(e: any) {
-  //   console.log('afterChange');
-  // }
-  // beforeChange(e: any) {
-  //   console.log('beforeChange');
-  // }
-  // handleDayClick(eventInfo: any): void {
-  //   let selectedEvents:any = eventInfo.day.events;
+  // handleDayClick(eventInfo:any){
+  //   console.log(eventInfo.day.events,'data')
+  //   const selectedEvents = this.events.filter(
+  //     (event) =>
+  //       event.start.getDate() === eventInfo.day.events.start.getDate() &&
+  //       event.start.getMonth() === eventInfo.day.events.start.getMonth() &&
+  //       event.start.getFullYear() === eventInfo.day.events.start.getFullYear()
+  //   );
+
   //   if (selectedEvents.length > 0) {
-  //     this.selectedMonthRecords = selectedEvents;
-  //     console.log( this.selectedMonthRecords,' this.selectedMonthRecords')
+  //     let selectedIndex = selectedEvents.findIndex(
+  //       (event) => event === eventInfo.event
+  //     );
+  //     selectedIndex = (selectedIndex + 1) % selectedEvents.length;
+  //     this.selectedEvent = selectedEvents[selectedIndex];
   //     this.openEventModal();
   //   }
   // }
+  addSlide() {
+    this.events.push(this.eventModalCarousel);
+  }
+  removeSlide() {
+    this.events.length = this.events.length - 1;
+  }
+  slickInit(e: any) {
+    console.log('slick initialized');
+  }
+  breakpoint(e: any) {
+    console.log('breakpoint');
+  }
+  afterChange(e: any) {
+    console.log('afterChange');
+  }
+  beforeChange(e: any) {
+    console.log('beforeChange');
+  }
+  handleDayClick(eventInfo: any): void {
+    let selectedEvents: any = eventInfo.day.events;
+    if (selectedEvents.length > 0) {
+      this.selectedMonthRecords = selectedEvents;
+      console.log(this.selectedMonthRecords, ' this.selectedMonthRecords')
+      this.openEventCarousalModal();
+    }
+  }
   openEventModal(): void {
     this.dialog.open(this.eventModal, {
+      width: '350px',
+      position: { top: '150px' },
+      disableClose: true,
+    });
+  }
+  openEventCarousalModal(): void {
+    this.dialog.open(this.eventModalCarousel, {
       width: '350px',
       position: { top: '150px' },
       disableClose: true,
@@ -286,10 +295,10 @@ export class EventsListComponent implements OnInit {
     this.route.navigate(["events/createEvents"]);
   }
 
-  navigateToBirthdays(){
+  navigateToBirthdays() {
     this.route.navigate(["events/birthdaysList"]);
   }
-  navigateToEventsList(){
+  navigateToEventsList() {
     this.route.navigate(["events/eventsListView"]);
   }
 }
