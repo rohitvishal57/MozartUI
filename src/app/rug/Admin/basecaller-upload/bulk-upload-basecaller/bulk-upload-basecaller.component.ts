b51acd66d39d3cc8eedc5fc07ee5bede0c3b67a7
arvind-monocept <arvindsubhashsutkar@monocept.com>
2025-01-16 12:27:14 +0530

    bulk upload error msg issue fixed

--- src/app/rug/Admin/basecaller-upload/bulk-upload-basecaller/bulk-upload-basecaller.component.ts
+++ src/app/rug/Admin/basecaller-upload/bulk-upload-basecaller/bulk-upload-basecaller.component.ts
@@ -106,7 +106,7 @@ export class BulkUploadBasecallerComponent {
         } else {
           this.toast.error({
             detail: "Error",
-            summary: "Unexpected response format. Please try again.",
+            summary: "Not Inserted. The uploaded file is missing the required column Domain Id. Please ensure that this column is included and correctly labeled in your file.",
             duration: 5000,
           });
           this.fileList = [];
