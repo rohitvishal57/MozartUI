9977673c98df9f6a011a65bdbfd66371448d0d95
padmavathi-monocept <padmavathi@monocept.com>
2025-01-22 18:17:17 +0530

    dynamically sum insured values changing based on plan for personal accident cover

--- src/app/yatra/yatra/yatra.component.ts
+++ src/app/yatra/yatra/yatra.component.ts
@@ -2528,7 +2528,194 @@ export class YatraComponent {
     }
   }
 
-
+  ActiveSecureSIvalues:any[] =[
+    {
+      "variantType": null,
+      "coverType": "PA",
+      "planType": "P1",
+      "productId": "ASPA",
+      "isPos": "0",
+      "isCabAgent": null,
+      "siList": [
+          {
+              "name": "500000",
+              "label": "500000",
+              "value": 500000
+          },
+          {
+              "name": "600000",
+              "label": "600000",
+              "value": 600000
+          },
+          {
+              "name": "700000",
+              "label": "700000",
+              "value": 700000
+          }
+      ],
+      "productType": "AS"
+  },  
+    {
+        "variantType": "null",
+        "coverType": "PA",
+        "planType": "P2",
+        "productId": "ASPA",
+        "isPos": "0",
+        "isCabAgent": null,
+        "siList":   [        
+          {
+          "name": "700000",
+          "label": "700000",
+          "value": 700000
+      }],
+        "productType": "AS"
+    },
+    {
+        "variantType": "null",
+        "coverType": "PA",
+        "planType": "P3",
+        "productId": "ASPA",
+        "isPos": "0",
+        "isCabAgent": null,
+        "siList": "[500000,600000,700000,800000,900000,1000000,1500000,2000000,2500000,3000000,4000000,5000000,10000000,15000000,20000000,50000000]",
+        "productType": "AS"
+    },
+    {
+        "variantType": "null",
+        "coverType": "PA",
+        "planType": "P4",
+        "productId": "ASPA",
+        "isPos": "0",
+        "isCabAgent": null,
+        "siList": "[500000,600000,700000,800000,900000,1000000,1500000,2000000,2500000,3000000,4000000,5000000,10000000,15000000,20000000,50000000,100000000]",
+        "productType": "AS"
+    },
+    {
+        "variantType": "null",
+        "coverType": "PA",
+        "planType": "P5",
+        "productId": "ASPA",
+        "isPos": "0",
+        "isCabAgent": null,
+        "siList": "[1000000,1500000,2000000,2500000,3000000,4000000,5000000,10000000,15000000,20000000,50000000,75000000,100000000,150000000,200000000]",
+        "productType": "AS"
+    },
+    {
+        "variantType": "null",
+        "coverType": "PA",
+        "planType": "P1",
+        "productId": "ASPA",
+        "isPos": "1",
+        "isCabAgent": null,
+        "siList": "[500000,600000,700000,800000,900000,1000000,1500000,2000000,2500000,3000000]",
+        "productType": "AS"
+    },
+    {
+        "variantType": "null",
+        "coverType": "PA",
+        "planType": "P2",
+        "productId": "ASPA",
+        "isPos": "1",
+        "isCabAgent": null,
+        "siList": "[500000,600000,700000,800000,900000,1000000,1500000,2000000,2500000,3000000]",
+        "productType": "AS"
+    },
+    {
+        "variantType": "null",
+        "coverType": "PA",
+        "planType": "P3",
+        "productId": "ASPA",
+        "isPos": "1",
+        "isCabAgent": null,
+        "siList": "[500000,600000,700000,800000,900000,1000000,1500000,2000000,2500000,3000000]",
+        "productType": "AS"
+    },
+    {
+        "variantType": "null",
+        "coverType": "PA",
+        "planType": "P4",
+        "productId": "ASPA",
+        "isPos": "1",
+        "isCabAgent": null,
+        "siList": "[500000,600000,700000,800000,900000,1000000,1500000,2000000,2500000,3000000]",
+        "productType": "AS"
+    },
+    {
+        "variantType": "null",
+        "coverType": "PA",
+        "planType": "P5",
+        "productId": "ASPA",
+        "isPos": "1",
+        "isCabAgent": null,
+        "siList": "[1000000,1500000,2000000,2500000,3000000]",
+        "productType": "AS"
+    }
+  ]
+  getSumInsuredValues(event: any, parentcontrol: any, otherControl: any, agentCode: string) {
+    const isPos = agentCode.startsWith('pos') ? 1 : 0;
+    const coverTypeMap: { [key: string]: string } = {
+      accident: 'PA',
+      criticalIllness: 'CI',
+      cancerSecure: 'CC'
+    };
+  
+    const coverType = coverTypeMap[parentcontrol.name];
+    console.log("Cover Type:", coverType);
+    const data = JSON.parse(event.target.value);
+    if (coverType) {
+      const filteredValues = this.ActiveSecureSIvalues.filter(item =>
+        item.planType === data.name && 
+        item.isPos === isPos.toString() &&
+        item.coverType === coverType
+      );
+      console.log("Filtered Values:", filteredValues);
+      console.log(filteredValues[0].siList);
+      if (filteredValues.length > 0) {
+        otherControl.options = (filteredValues[0].siList);        
+        console.log("Assigned Sum Insured Values to otherControl:", otherControl.options);
+      } else {
+        console.log("No matching values found in ActiveSecureSIvalues.");
+        otherControl.options = []; // Clear options if no match is found
+      }
+    } else {
+      console.log("Invalid coverType for parentcontrol.name:", parentcontrol.name);
+    }
+  }
+//   getNatureOfWorkByOccupation(event:any){
+//     const parsedValue = JSON.parse(event.target.value);
+//       this.form.formSections.forEach((section: any) => {
+//         section.formControls.forEach((formControl: any) => {
+//           if (formControl.subControls) {
+//             formControl.subControls.forEach((subControl: any) => {
+//               if (subControl.innerSubControls) {
+//                 subControl.innerSubControls.forEach((innerSubControls: any) => {
+//                   if (innerSubControls.coreControls) {
+//                     innerSubControls.coreControls.forEach((coreControl: any) => {
+//                       if(parsedValue.name === 'Retired' && coreControl.name == "occupationRisk"){
+//                         coreControl.options =[{
+//                           "id": "1",
+//                           "value": "O464",
+//                           "name": "Retired"
+//                         }]
+//                       }else if(coreControl.name == "occupationRisk"){
+//                           this.yatraService.getNatureOfDuty().subscribe({
+//                             next: (res: any) => {
+//                               coreControl.options = res.data;
+//                             },
+//                             error: (err: any) => {
+//                               console.error(err);
+//                             }
+//                           });
+//                       }
+//                     });
+//                   }
+//                 });
+//               }
+//             });
+//           }
+//         });
+//       });              
+// }
 
   onInputChange(event: any, control: any, parentControl: any = null, index: any = null, subControl: any = null, innerControl: any = null, indexj: any = null, benefitControl: any = null) {
     console.log(event, control, parentControl, index, subControl, innerControl, indexj);
@@ -2576,7 +2763,47 @@ export class YatraComponent {
       //   }
       // }
     }
-
+    if(this.formData.productName=== 'Active Secure' && control.name=== 'addOnDetails'){
+      const parsedValue = JSON.parse(event.target.value);
+      const agentCode =this.agentCode
+      this.form.formSections.forEach((section: any) => {
+        section.formControls.forEach((formControl: any) => {
+          if (formControl.subControls) {
+            formControl.subControls.forEach((subControl: any) => {
+              if (subControl.innerSubControls) {
+                subControl.innerSubControls.forEach((innerSubControls: any) => {
+                  if (innerSubControls.coreControls) {
+                    innerSubControls.coreControls.forEach((coreControl: any) => {
+                      if(parsedValue.name === 'Retired' && coreControl.name == "occupationRisk"){
+                        coreControl.options =[{
+                          "id": "1",
+                          "value": "O464",
+                          "name": "Retired"
+                        }]
+                      }else if(coreControl.name == "occupationRisk"){
+                          this.yatraService.getNatureOfDuty().subscribe({
+                            next: (res: any) => {
+                              coreControl.options = res.data;
+                            },
+                            error: (err: any) => {
+                              console.error(err);
+                            }
+                          });
+                      }
+                      // Check the condition inside coreControls
+                      else if (coreControl.name === "addOnSumInsured") {
+                        console.log("Core Control:", coreControl);
+                        this.getSumInsuredValues(event, parentControl, coreControl, agentCode);
+                      }
+                    });
+                  }
+                });
+              }
+            });
+          }
+        });
+      });        
+  }
     if (control.name == 'physicalcopy' && control.type == 'radio') {
       const selectedValue = this.dynamicFormGroup.get(control.name)?.value;
       this.form.formSections.forEach((section: any) => {
